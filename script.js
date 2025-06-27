b06af958fae5fec44bbd387d90db42c7e9829fb3
Yasoob <kamiliyasoob@gmail.com>
2025-06-27 14:16:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("cphMain_ddlTopic").selectedIndex = 1;
+document.getElementById("cphMain_txtDuration").value = "60";
+document.getElementById("cphMain_ddlRating").selectedIndex = 5;
+document.getElementById("cphMain_txtComments").value = "Great session!";
+document.getElementById("cphMain_btnSubmit").click();
