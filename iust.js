b2cff7acadd2813b660615d6ea6a483c8a71de79
Yasoob <kamiliyasoob@gmail.com>
2025-06-27 20:42:45 +0530

    Update and rename script.js to iust.js

--- iust.js
+++ iust.js
@@ -1,5 +1,12 @@
+//part1
 document.getElementById("cphMain_ddlTopic").selectedIndex = 1;
+document.getElementById("cphMain_ddlTopic").dispatchEvent(new Event('change'));
+
+//part2
 document.getElementById("cphMain_txtDuration").value = "60";
 document.getElementById("cphMain_ddlRating").selectedIndex = 5;
 document.getElementById("cphMain_txtComments").value = "Great session!";
 document.getElementById("cphMain_btnSubmit").click();
+
+// run the two parts separately, otherwise the server might start feeling a little nervous under the pressure!
+// plus if you don't get what this script is, it's not for you
